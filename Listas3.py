c412fa55db021267ab6f78cb892820bd718cc782
ELDLC18 <josuefabrizioelias@gmail.com>
2025-04-11 21:01:42 -0500

    Listas Agregadas

---
+++ Listas3.py
@@ -0,0 +1,11 @@
+a = [2*x for x in range(1, 11)]
+print (a)
+
+i = [(2*n) +1 for n in range(0,5)]
+print(i)
+
+print([n for n in range(1,10,2)])
+
+print([0 if x % 3 == 0 else x for x in range(1, 10, 2)])
+
+print("hola\n")
